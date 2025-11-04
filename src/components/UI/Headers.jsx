f6daeb402cb3336d7b16756ad1db3e73b77115f4
Tanvir <tanvirislamm.39@gmail.com>
2025-11-04 16:49:44 +0600

    Initial commit

--- src/components/UI/Headers.jsx
+++ src/components/UI/Headers.jsx
@@ -6,7 +6,7 @@ export const Headers = () => {
     <header>
     <nav className="navbar navbar-expand-lg bg-body-tertiary fixed-top">
   <div className="container">
-    <NavLink to="/home" className="navbar-brand">WorldAtlas</NavLink >
+    <NavLink to="/" className="navbar-brand">WorldAtlas</NavLink >
     <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
@@ -18,7 +18,7 @@ export const Headers = () => {
       <div className="offcanvas-body">
         <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
           <li className="nav-item">
-            <NavLink to="/home" className="nav-link active" aria-current="page" >Home</NavLink >
+            <NavLink to="/" className="nav-link active" aria-current="page" >Home</NavLink >
           </li>
           <li className="nav-item">
             <NavLink to="/about" className="nav-link" >About</NavLink >
